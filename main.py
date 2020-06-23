913dd18bf16c9aaeb7d73bca19854b29064823bd
Brian Riedlinger <64873347+brianr0922@users.noreply.github.com>
2020-06-23 17:20:07 -0400

    Add files via upload
    Finished list and accordions

--- main.py
+++ main.py
@@ -36,22 +36,31 @@ def pic_criteria(rov_name):
 def imgcrit():
 	rov_name = request.args.get('rov_name')
 	form_date = request.form['date']
-	form_cam = request.form['optcam']
+	try:
+		form_cam = request.form['optcam']
+	except:
+		form_cam = ""
 	return redirect(f'/list.html?rov_name={rov_name}&img_date={form_date}&sel_cam={form_cam}')
 
 
 @app.route('/list.html')
 def img_list():
+	opts=""
 	rov_name = request.args.get('rov_name')
 	img_date = request.args.get('img_date')
 	sel_cam = request.args.get('sel_cam')
-	img_list = requests.get(f'https://api.nasa.gov/mars-photos/api/v1/rovers/{rov_name}/photos?earth_date={img_date}&camera={sel_cam}&api_key={api_key}')
-	if img_list.text == "":
+	opts = "earth_date=" + img_date
+	if sel_cam != "":
+		opts += "&camera=" + sel_cam
+	opts += "&api_key=" + api_key
+	emsg = ""
+	api_list = requests.get(f'https://api.nasa.gov/mars-photos/api/v1/rovers/{rov_name}/photos?{opts}')
+	if api_list.text == "":
 		emsg = 'No images for that camera and date.  Please try again.'
 		return redirect(f'/{rov_name}.html?emsg="No images for that camera and date.  Please try again."')
 	else:
-		img_list = eval(img_list.text)
-		img_list = img_list["photos"]
+		api_list = eval(api_list.text)
+		img_list = api_list["photos"]
 		max_rows = len(img_list)
 
 		rend_list = {}
@@ -66,7 +75,7 @@ def img_list():
 				}
 			rend_list.update({i:dictval})
 
-		return render_template('list.html', rname=rov_name, ilist=rend_list)
+		return render_template('list.html', rname=rov_name, ilist=rend_list, alist=api_list)
 
 
 if __name__ == '__main__':
